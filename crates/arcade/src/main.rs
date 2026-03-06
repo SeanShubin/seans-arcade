a57bebe8864d42d5a24dc3072e35d439e9665be8
SeanShubin <seanshubin@gmail.com>
2026-03-05 19:57:24 -0800

    skeletons for each application

---
+++ crates/arcade/src/main.rs
@@ -0,0 +1,23 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Text::new("arcade"),
+        TextFont::from_font_size(48.0),
+        TextColor::WHITE,
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Percent(50.0),
+            left: Val::Percent(50.0),
+            ..default()
+        },
+    ));
+}
---
+++ scripts/run-arcade.cmd
@@ -0,0 +1,2 @@
+@echo off
+cd /d "%~dp0.." && cargo run -p arcade
